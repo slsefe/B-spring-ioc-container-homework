4393898406dd3bc51ab63aab70485ffedb57722b
Yin Bai <17693374267@163.com>
2021-03-02 18:05:06 +0800

    [baiyin] fix: repair circular dependencies with @PostConstruct.

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,14 +1,19 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Foo {
 
+    @Autowired
     private Bar bar;
 
-    public Foo(Bar bar) {
-        this.bar = bar;
+    @PostConstruct
+    public void init() {
+        this.bar.setFoo(this);
     }
 
     public void hi() {
